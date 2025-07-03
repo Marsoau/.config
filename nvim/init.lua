0ba1e875cdafc4bc6d283059b2885dbb93393823
Marsoau <koteika5063@gmail.com>
2025-07-03 16:59:29 +0200

    fix undotree not working

--- nvim/init.lua
+++ nvim/init.lua
@@ -961,7 +961,7 @@ require("lazy").setup({
 	},
 })
 
-vim.g.undotree_DiffCommand = "FC"
+vim.g.undotree_DiffCommand = "diff"
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
